bb4bcef28ff0baed31a40190e379c2100d9093fb
Oskar Blom <oskar.blom@gmail.com>
2011-02-07 21:56:06 +0100

    Added basic (and untested) game logic

--- webapp.py
+++ webapp.py
@@ -5,7 +5,7 @@ import md5
 import hashlib
 
 app = Flask(__name__)
-repo = GameRepository()
+repo = GameService()
 
 @app.route("/")
 def main():
@@ -15,21 +15,41 @@ def main():
 def create_game():
     return "Hello World!"
 
-class GameRepository(object):
+class GameService(object):
+
+    MAX_TRIES = 10
 
     def __init__(self):
         self.db = Connection().game_db
+
+    def add_player_to_game(self, channel):
+        game = self.db.games.find_one({"channel": channel})
+        game["participants"] = 2
+        self.db.games.insert(game)
 
     def create_game(self, word):
         channel = hashlib.md5(str(time.time())).hexdigest()
         game = {
             "word": word,
             "channel": channel,
-            "isrunning": True,
-            "guesses": []
+            "status": "running",
+            "guesses": [],
+            "participants": 1
         }
         self.db.games.insert(game)
         return channel, game
+
+    def guess(self, channel, letter):
+        game = self.db.games.find_one({"channel" : channel})
+        current_guesses = len(game["guesses"])
+        if letter in game["guesses"]:
+            game["status"] = "duplicate":
+        else:
+            if current_guesses + 1 == self.MAX_TRIES:
+                game["status"] = "over"
+            else:
+                game["guesses"].append(letter)
+        return game
 
 if __name__ == "__main__":
     app.run(debug=True)

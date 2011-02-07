1271c4c4d4e60263c026317de16cd77485f830f8
Oskar Blom <oskar.blom@gmail.com>
2011-02-07 17:40:20 +0100

    Moved static index.html into templates. Made webapp render it instead

--- webapp.py
+++ webapp.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def main():
+    return render_template("index.html")
+
+@app.route("/api/creategame")
+def create_game():
     return "Hello World!"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)

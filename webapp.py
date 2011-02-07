f33ce068833fa31d107f21e8e37d92ecef87c324
Oskar Blom <oskar.blom@gmail.com>
2011-02-07 21:21:35 +0100

    Added mongo db game repository

--- webapp.py
+++ webapp.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
+from pymongo import connection
+import time
+import md5
+import hashlib
 
 app = Flask(__name__)
+repo = GameRepository()
 
 @app.route("/")
 def main():
@@ -9,6 +14,22 @@ def main():
 @app.route("/api/creategame")
 def create_game():
     return "Hello World!"
+
+class GameRepository(object):
+
+    def __init__(self):
+        self.db = Connection().game_db
+
+    def create_game(self, word):
+        channel = hashlib.md5(str(time.time())).hexdigest()
+        game = {
+            "word": word,
+            "channel": channel,
+            "isrunning": True,
+            "guesses": []
+        }
+        self.db.games.insert(game)
+        return channel, game
 
 if __name__ == "__main__":
     app.run(debug=True)

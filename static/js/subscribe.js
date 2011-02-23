cde4dce7bceccc8bd3aaca6bee609e771a48ecfd
Oskar Blom <oskar.blom@gmail.com>
2011-02-23 07:58:23 +0100

    Fixed syntax error

--- static/js/subscribe.js
+++ static/js/subscribe.js
@@ -44,7 +44,7 @@ function EventHandler(hangman, statusContainer, wordStateContainer, observer) {
     var.stringFromList = function(list) {
         var wd = "";
         $.each(list, function(i){
-            wd = data.word_state[i];
+            wd = list[i];
         });
         return wd;
     };

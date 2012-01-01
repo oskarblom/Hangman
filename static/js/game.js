ef7f4d6fd974382bf6fbfeb7c77d90467c3b04f6
Oskar Blom <oskar.blom@gmail.com>
2012-01-01 21:55:03 +0100

    Added new js-file for event-related stuff

---
+++ static/js/game.js
@@ -0,0 +1,18 @@
+var GameEvents = (function() {
+    var Game = function(eventHandler) {
+        this.handler = eventHandler;
+        this.jug = new Juggernaut;
+    };
+    Game.prototype.subscribe = function(channel) {
+        this.jug.subscribe(channel,  function(data) {
+            console.log("data"); 
+        });
+    };
+    return {
+        "listen" : function(channel, eventHandler) {
+            console.log("yes");
+            new Game(eventHandler).subscribe(channel);
+        }
+    }
+}());
+

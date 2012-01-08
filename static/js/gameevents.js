2f40177758feea33844cac15a16fef5d5ee0cf34
Oskar Blom <oskar.blom@gmail.com>
2012-01-08 20:46:42 +0100

    added basic support for the different events sent from the server

--- static/js/gameevents.js
+++ static/js/gameevents.js
@@ -4,13 +4,29 @@ var gameEvents = (function() {
         this.jug = new Juggernaut;
     };
     Game.prototype.subscribe = function(channel) {
+        hangman = new Hangman(document.getElementById("hangman-canvas"));
         this.jug.subscribe(channel, function(data) {
-            console.log(data); 
+            gameEvent = eval(data);
+            console.log(gameEvent); 
+            switch (gameEvent.state) {
+                case "RUNNING":
+                    break;
+                case "CORRECT_GUESS":
+                    break;
+                case "INCORRECT_GUESS":
+                    hangman.drawNextPart();
+                    break;
+                case "OVER_SAVED":
+                    break;
+                case "OVER_HUNG":
+                    break;
+                default:
+                    break;
+            }
         });
     };
     return {
         "listen" : function(channel, eventHandler) {
-            console.log("yes");
             new Game(eventHandler).subscribe(channel);
         }
     }
